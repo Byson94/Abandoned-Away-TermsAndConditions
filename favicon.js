0d28ce8213671997b5b6e5462dd6cb3e9d074f7e
Byson94 <nilaybyju2012@gmail.com>
2024-07-20 11:37:21 +0530

    Create favicon.js

---
+++ favicon.js
@@ -0,0 +1,9 @@
+// favicon.js
+document.addEventListener("DOMContentLoaded", function() {
+    var link = document.createElement('link');
+    link.rel = 'icon';
+    link.type = 'image/png';
+    link.href = 'https://raw.githubusercontent.com/Byson94/Lonely-Orbit-TermsAndConditions/main/Icon.png';
+
+    document.head.appendChild(link);
+});
